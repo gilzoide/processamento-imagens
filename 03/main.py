35e9ac4bd6a5a0867b6772ce8d3386596559d311
gilzoide <gilzoide@gmail.com>
2017-05-04 17:15:17 -0300

    T3, falta consertar os complexo do ifft

--- 03/main.py
+++ 03/main.py
@@ -9,14 +9,21 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 from scipy import signal
+from scipy.fftpack import fft, ifft
 
+def NFS(s):
+    return np.absolute(s) / (2 * len(s))
+
+def loPass(c, Ffft, Gfft):
+    T = c * np.argmax(np.absolute(Gfft))
+    return np.array([a if a < T else 0 for a in Ffft])
 
 def main():
     # 1 - Input reading
     signal_name = input()
     s = float(input())
     c = float(input())
-    show = input() == '1'
+    show = input().startswith('1')
     # 2 - Read the signal
     F = np.fromfile(signal_name, dtype=np.int32)
     # 3 - Apply the Gaussian Windowing
@@ -24,15 +31,38 @@ def main():
     window = signal.gaussian(len(F), sigma)
     G = F * window
     # 4 - FFT(F)
+    Ffft = fft(F)
     # 5 - FFT(G)
+    Gfft = fft(G)
     # (Show flag == True) 6 - Plot F, G, FFT(F), FFT(G)
+    if show:
+        fig = plt.figure()
+        a1 = fig.add_subplot(221)
+        a2 = fig.add_subplot(222)
+        a3 = fig.add_subplot(223)
+        a4 = fig.add_subplot(224)
+        a1.plot(F)
+        a2.plot(NFS(Ffft)[:len(F) // 2])
+        a3.plot(G)
+        a4.plot(NFS(Gfft)[:len(G) // 2])
+        plt.show()
+    # 7 - Low pass filter
+    filteredFfft = loPass(c, Ffft, Gfft)
+    # 8 - IFFT(filteredF) TODO: consertar o resultado (que tá complexo)
+    filteredF = ifft(filteredFfft)
+    # (Show flag == True) 9 - Plot F, filteredF
     if show:
         fig = plt.figure()
         a1 = fig.add_subplot(211)
         a2 = fig.add_subplot(212)
         a1.plot(F)
-        a2.plot(G)
+        a2.plot(filteredF)
         plt.show()
+    # 10 - outputs
+    print(np.argmax(np.absolute(Ffft)))
+    print(np.argmax(np.absolute(Gfft)))
+    print(np.amax(F))
+    print(np.amax(filteredF))
 
 if __name__ == '__main__':
     main()
